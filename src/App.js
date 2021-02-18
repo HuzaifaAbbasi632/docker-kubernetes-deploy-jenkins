1790c4d8462a79c4062da729682cb1ac954cf3e9
Muhammad Huzaifa <muhammad.huzaifa@aksa.com.pk>
2021-02-18 13:06:14 +0500

    new deployment

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 style={{color:'red'}}>Waqas</h1>
+      <h1 style={{color:'black'}}>Ne Deployment</h1>
     </div>
   );
 }
