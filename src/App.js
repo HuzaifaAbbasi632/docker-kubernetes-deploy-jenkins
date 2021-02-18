ad7f70aa8879558fa1ee42ce141a000cf97b239f
Muhammad Huzaifa <muhammad.huzaifa@aksa.com.pk>
2021-02-18 19:48:05 +0500

    Final

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 style={{color:'black'}}>Deployment Test</h1>
+      <h1 style={{color:'black'}}>Demo Application</h1>
     </div>
   );
 }
