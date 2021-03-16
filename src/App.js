0104f7771a5bf36c5552586f8413bb38a35c5f4b
Muhammad Huzaifa <muhammad.huzaifa@aksa.com.pk>
2021-03-16 13:06:35 +0500

    Sada

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 style={{color:'black'}}>Demo Application</h1>
+      <h1 style={{color:'black'}}>Demo Application Test</h1>
     </div>
   );
 }
