d3e37c8d1b21d51846850b17070314f4582b8487
Muhammad Huzaifa <muhammad.huzaifa@aksa.com.pk>
2021-03-16 13:13:31 +0500

    CI/CD

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 style={{color:'black'}}>Demo Application Test</h1>
+      <h1 style={{color:'black'}}>CI/CD In Action</h1>
     </div>
   );
 }
