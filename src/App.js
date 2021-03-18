002661607a0df8fc4cf565b7bf4f9d6b14b66610
Muhammad Huzaifa <muhammad.huzaifa@aksa.com.pk>
2021-03-18 09:44:20 +0500

    App Control

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 style={{color:'black'}}>CI/CD In Action</h1>
+      <h1 style={{color:'black'}}>App Controlled by Jenkins</h1>
     </div>
   );
 }
