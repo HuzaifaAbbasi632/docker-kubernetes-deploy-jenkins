499a08a9b95b27ac21790b1b51834c6879191362
Muhammad Huzaifa <muhammad.huzaifa@aksa.com.pk>
2021-03-18 15:27:54 +0500

    SADAS

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 style={{color:'black'}}>App Controlled by Jenkins</h1>
+      <h1 style={{color:'black'}}>Version 1.0.3</h1>
     </div>
   );
 }
