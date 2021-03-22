cf7c13044d711c456eff3f4e6a278b35fa882d98
Muhammad Huzaifa <muhammad.huzaifa@aksa.com.pk>
2021-03-22 10:09:41 +0500

    Version 1.0.9

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 style={{color:'black'}}>Version 1.0.6</h1>
+      <h1 style={{color:'black'}}>Version 1.0.9</h1>
     </div>
   );
 }
